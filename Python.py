eedc9706ba6260788e4bf33d27c5ed7713715698
Kirill <Mrgrenn@gmail.com>
2023-05-27 23:00:09 +0300

    done lesson 1

---
+++ Python.py
@@ -0,0 +1,20 @@
+"""
+a = int(input ())
+b = int(input ())
+c = int(input ())
+
+print(a+b+c)
+"""
+"""
+r = int(input("VVedite radius kruga "))
+
+print ((r*r)*3.4)
+"""
+"""
+r = int(input("vvedie chislo "))
+
+if r>= 1000 and r <=9999:
+    print ("YES")
+else:
+    print ("No")
+"""
